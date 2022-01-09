1a390d2b48fa8e1bd0ebda268cfce0d7f9941b7d
barthr <info@bartfokker.com>
2022-01-09 19:39:08 +0100

    Filter in single go + href to own tool

--- main.js
+++ main.js
@@ -9,15 +9,19 @@ const cleanDomainInput = (input) => {
     return input.match(/[a-zA-Z0-9]+/g).join("").toLowerCase();
 }
 
+const validDomain = (input, tld) => {
+    return input.length > tld.length
+        && input.endsWith(tld)
+        && input.slice(0, -tld.length).length > 1
+}
+
 const findDomainNames = input => {
     if (input == null)
         return []
 
     input = cleanDomainInput(input)
     return tlds
-        .filter(tld => input.length > tld.length)
-        .filter(tld => input.endsWith(tld))
-        .filter(tld => input.slice(0, -tld.length).length > 1)
+        .filter(tld => validDomain(input, tld))
         .map(tld => input.slice(0, -tld.length) + "." + tld)
 }
 
@@ -50,7 +54,15 @@ domainInput.addEventListener("keydown", ev => {
     if (ev.key === "Enter") handleInputEvent()
 })
 
+const domain = new URLSearchParams(window.location.search).get("domain");
+if (domain != null) {
+    domainInput.value = domain;
+    handleInputEvent();
+}
 
-
+document.querySelectorAll("[data-url]").forEach(el => {
+    console.log(el)
+    el.setAttribute("href", `?domain=${el.getAttribute("data-url")}`)
+});
 
 
